2d5a7b75698b5c2ef298b5ea79ee9f8792fc285d
PanVat <filip.podeszwa@seznam.cz>
2024-05-22 11:25:31 +0200

    2) Vytvoření základních souborů

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo('server');
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
+app.use(express.static('public'));
+
+io.on('connection', () => {
+    console.log("Připojil se uživatel");
+    server.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+app.listen(PORT, () => {
     console.log("Server naslouchá na portu " + PORT);
 });
